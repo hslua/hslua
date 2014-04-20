296ebfaa6e102e4cbd0e72ad7b30e3d944ce37b5
osa1 <omeragacan@gmail.com>
2014-04-20 16:35:32 +0300

    add a hello world example with lua version printing(to be used for debugging purposes)

---
+++ test/hello.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import qualified Scripting.Lua as Lua
+
+main :: IO ()
+main = do
+    l <- Lua.newstate
+    Lua.openlibs l
+    Lua.getfield l Lua.globalsindex "print"
+    Lua.pushstring l "Hello from"
+    Lua.getfield l Lua.globalsindex "_VERSION"
+    Lua.call l 2 0
+    Lua.close l
