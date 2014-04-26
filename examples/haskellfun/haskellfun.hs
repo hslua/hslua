b47344757231e8a513e233b681c3fe7f68408160
osa1 <omeragacan@gmail.com>
2014-04-26 14:47:09 +0300

    add higher level haskell function mapping example

---
+++ examples/haskellfun/haskellfun.hs
@@ -0,0 +1,25 @@
+
+-- An example with higher-level Haskell functions. Haskell functions are
+-- wrapped by hslua automatically for ensuring argument types and nubmers
+-- and passing arguments from Lua stack to Haskell functions. Return values
+-- are also handled by hslua automatically(so you don't put return value to
+-- stack manually).
+
+import Scripting.Lua as Lua
+import Prelude
+
+main :: IO ()
+main = do
+    l <- newstate
+    openlibs l
+    registerhsfunction l "concat" concat'
+    registerhsfunction l "pow" pow
+    loadfile l "haskellfun.lua"
+    call l 0 0
+    close l
+
+concat' :: String -> String -> IO String
+concat' s1 s2 = return $ s1 ++ s2
+
+pow :: Double -> Double -> IO Double
+pow d1 d2 = return $ d1 ** d2
---
+++ examples/haskellfun/haskellfun.lua
@@ -0,0 +1,4 @@
+print(concat("hello", " world!"))
+print(pow(3.2, 5))
+print(pow("wrong"))
+print(pow(3))
