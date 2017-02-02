dfc22208a00c70711665bde6dd0de8d7d802dfda
Albert Krewinkel <albert@zeitkraut.de>
2017-02-02 08:12:55 +0100

    Improve tests for the Scientific StackValue instance

--- test/AesonSpec.hs
+++ test/AesonSpec.hs
@@ -6,9 +6,10 @@ License     :  MIT
 Tests for Aeson–Lua glue.
 -}
 import Control.Monad (forM_)
-import Data.Char (toLower)
+import Data.AEq ((~==))
+import Data.Scientific (Scientific, toRealFloat)
 import Scripting.Lua.Aeson (StackValue)
-import qualified Data.Scientific as Sci
+
 import qualified Scripting.Lua as Lua
 
 import Test.Hspec
@@ -24,16 +25,30 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Value component" $ do
-    describe "Boolean" $ do
-      it "can be converted to a lua boolean" $ property $
-        \x -> assert =<< luaTest "type(x) == 'boolean'" [("x", x::Bool)]
-      it "can be read from a lua boolean on the stack" $ property $
-        \x -> assert =<< luaReturnEqual (map toLower $ show (x::Bool)) x
     describe "Scientific" $ do
       it "can be converted to a lua number" $ property $
-        \x -> assert =<< luaTest "type(x) == 'number'" [("x", x::Sci.Scientific)]
-      it "can be read from a lua number on the stack" $ property $
-        \x -> assert =<< luaReturnApprox (show x) x
+        \x -> assert =<< luaTest "type(x) == 'number'" [("x", x::Scientific)]
+      it "can be round-tripped through the stack (lossy)" $ property $
+        \x -> assertRoundtripApprox (x::Scientific)
+
+assertRoundtripApprox :: Scientific -> IO ()
+assertRoundtripApprox x = do
+  y <- roundtrip x
+  let xdouble = toRealFloat x :: Double
+  let ydouble = toRealFloat y :: Double
+  assert (xdouble ~== ydouble)
+
+roundtrip :: (StackValue a) => a -> IO a
+roundtrip x = do
+  lua <- Lua.newstate
+  Lua.push lua x
+  res <- Lua.peek lua (-1)
+  retval <- res `seq` case res of
+        Nothing -> error "could not read from stack"
+        Just y  -> do
+          return y
+  Lua.close lua
+  return retval
 
 luaTest :: StackValue a => String -> [(String, a)] -> IO Bool
 luaTest luaTestCode xs = do
@@ -41,28 +56,6 @@ luaTest luaTestCode xs = do
   forM_ xs $ \(var, value) ->
     Lua.push lua value *> Lua.setglobal lua var
   let luaScript = "function run() return (" ++ luaTestCode ++ ") end"
-  Lua.openlibs lua
-  _ <- Lua.loadstring lua luaScript "test script"
-  Lua.call lua 0 0
-  retval <- Lua.callfunc lua "run"
-  Lua.close lua
-  return retval
-
-luaReturnApprox :: String -> Sci.Scientific -> IO Bool
-luaReturnApprox valStr val = do
-  retVal <- Sci.toRealFloat <$> luaReturn valStr
-  let doubleVal = (Sci.toRealFloat val)::Double
-  return $ doubleVal == retVal
-
-luaReturnEqual :: (Eq a, StackValue a) => String -> a -> IO Bool
-luaReturnEqual valStr val = do
-  retVal <- luaReturn valStr
-  return $ val == retVal
-
-luaReturn :: StackValue a => String -> IO a
-luaReturn luaValue = do
-  lua <- Lua.newstate
-  let luaScript = "function run() return (" ++ luaValue ++ ") end"
   Lua.openlibs lua
   _ <- Lua.loadstring lua luaScript "test script"
   Lua.call lua 0 0
